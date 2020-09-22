8713c32625aaa49dde03ec61731e67a837f561c4
Ana-Lena Tappe <ana-lena.tappe@hu-berlin.de>
2020-09-22 11:47:41 +0200

    fixed plot mistake + testing python package making

---
+++ Test_Package/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mypackage',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='ana',
+author_email='testr@gmail.com',
+license='MIT',
+packages=['mypackage'],
+zip_safe=False)
---
+++ Test_Package/test.py
@@ -0,0 +1,4 @@
+from mypackage import power, average, SayHello
+SayHello("Ana")
+x=power(3,2)
+print("power(3,2) : ", x)
