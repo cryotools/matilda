6b0dadb767ad4593ddd879a84f1cccd8fc5b0a24
Phillip Schuster <susterph@login.cirrus>
2020-11-20 15:11:00 +0100

    NCDF subset script for Cirrus updated

--- Tools/extract_section_time.py
+++ Tools/extract_section_time.py
@@ -1,4 +1,11 @@
 import xarray as xr
-ds = xr.open_dataset('best_cosipy_output_no1_2000-20.nc')
-ds = ds.sel(time=slice('20110101T00:00', '20181231T23:00'))
-ds.to_netcdf('test.nc')
+input = '/data/projects/prime-SG/ERA5Land/Tien_Shan_Region/20200921_ERA5-Land_Tien_Shan_Region_2001_2019.nc'
+# input = '/data/projects/ebaca/data/input_output/input/ERA5/20200722_Karabatkak_ERA5L_1982_2019.nc'
+output_folder = '/data/projects/ebaca/data/input_output/input/ERA5/'
+output_file = '20200722_Karabatkak_ERA5L_1982_83.nc'
+
+ds = xr.open_dataset(input)
+ds = ds.sel(time=slice('20010101T00:00', '20021231T23:00'))
+ds.to_netcdf(output_folder + output_file)
+
+
