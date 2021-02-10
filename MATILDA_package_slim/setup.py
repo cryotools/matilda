5c211fda492dfa0b5fa4575fe28725c1f99286cf
Correct Name <analenatappe@gmail.com>
2021-02-10 15:42:47 +0100

    MATILDA slim

---
+++ MATILDA_package_slim/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(name='MATILDA_slim',
+version='0.1',
+description='Modeling wATer resources In gLacierizeD cAtchments',
+url='#',
+author='ana',
+author_email='testr@gmail.com',
+license='MIT',
+install_requires=['pandas' , 'xarray', 'matplotlib', 'numpy', 'scipy', 'datetime'], 
+packages=['MATILDA_slim'],
+zip_safe=False)
