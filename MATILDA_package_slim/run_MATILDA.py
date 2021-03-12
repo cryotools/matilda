ed7410a0041a5ed527ad5ad16b170fb7b7d1da4a
Phillip Schuster <phillip.schuster@posteo.de>
2021-03-12 14:11:20 +0100

    Minor changes MATILDA_slim

--- MATILDA_package_slim/run_MATILDA.py
+++ MATILDA_package_slim/run_MATILDA.py
@@ -5,12 +5,14 @@ This file may use the input files created by the COSIPY-utility "aws2cosipy" as 
 """
 ## import of necessary packages
 import pandas as pd
+from pathlib import Path
 from MATILDA_slim import MATILDA
+home = str(Path.home())
 
 ## Setting file paths and parameters
-working_directory = "/home/ana/Seafile/Ana-Lena_Phillip/data/"
-input_path_data = "/home/ana/Seafile/Ana-Lena_Phillip/data/input_output/input/ERA5/Tien-Shan/At-Bashy/"
-input_path_observations = "/home/ana/Seafile/Ana-Lena_Phillip/data/input_output/input/observations/bash_kaindy/"
+working_directory = home + "/Seafile/Ana-Lena_Phillip/data/"
+input_path_data = home + "/Seafile/Ana-Lena_Phillip/data/input_output/input/ERA5/Tien-Shan/At-Bashy/"
+input_path_observations = home + "/Seafile/Ana-Lena_Phillip/data/input_output/input/observations/bash_kaindy/"
 
 data_csv = "no182_ERA5_Land_2000_202011_no182_41_75.9_fitted.csv" # dataframe with columns T2 (Temp in Celsius), RRR (Prec in mm) and if possible PE (in mm)
 observation_data = "runoff_bashkaindy_04_2019-11_2020_temp_limit.csv" # Daily Runoff Observations in mm
@@ -42,4 +44,4 @@ output_MATILDA = MATILDA.MATILDA_plots(output_MATILDA, parameter)
 output_MATILDA = MATILDA.MATILDA_simulation(df, obs=obs, set_up_start='2018-01-01 00:00:00', set_up_end='2018-12-31 23:00:00',
                        sim_start='2019-01-01 00:00:00', sim_end='2020-11-01 23:00:00', freq="D", area_cat=46.232, area_glac=2.566,
                        ele_dat=3864, ele_glac=4042, ele_cat=3360, TT_snow=0, TT_rain=2)
-#output_MATILDA[4].show()
+output_MATILDA[4].show()
