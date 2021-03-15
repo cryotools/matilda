f481c32f7bfc7a9766b2f5bf6f256aada279b51c
Phillip Schuster <phillip.schuster@hu-berlin.de>
2021-03-15 16:25:35 +0100

    Debugged SpotPy Draft

---
+++ MATILDA_package_slim/venv/lib/python3.6/site-packages/xarray/plot/__init__.py
@@ -0,0 +1,16 @@
+from .dataset_plot import scatter
+from .facetgrid import FacetGrid
+from .plot import contour, contourf, hist, imshow, line, pcolormesh, plot, step
+
+__all__ = [
+    "plot",
+    "line",
+    "step",
+    "contour",
+    "contourf",
+    "hist",
+    "imshow",
+    "pcolormesh",
+    "FacetGrid",
+    "scatter",
+]
