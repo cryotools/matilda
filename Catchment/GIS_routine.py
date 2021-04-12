9470bf3e79822cccf0a3e3b830a971127cc5a716
Ana-Lena Tappe <ana-lena.tappe@hu-berlin.de>
2021-04-12 17:19:31 +0200

    GIS routine

--- Catchment/GIS_routine.py
+++ Catchment/GIS_routine.py
@@ -12,8 +12,12 @@ import warnings
 warnings.filterwarnings('ignore')
 
 ## Input
+working_directory = home + "/Seafile/SHK/Scripts/centralasiawaterresources/Catchment/"
 input_DEM = home + "/Seafile/Ana-Lena_Phillip/data/input_output/static/DEM/n43_e086_3arc_v2.tif"
-RGI_files = "/home/ana/Seafile/Tianshan_data/GLIMS/13_rgi60_CentralAsia/13_rgi60_CentralAsia.shp"
+RGI_files = home + "/Seafile/Tianshan_data/GLIMS/13_rgi60_CentralAsia/13_rgi60_CentralAsia.shp"
+ice_thickness_files = home + "/Seafile/Tianshan_data/01_original/"
+output_test = home + "/Desktop/Thickness"
+
 x, y = 86.82151540, 43.11473921 # pouring point
 ele_bands, ele_zones = 0,0
 
@@ -31,32 +35,42 @@ def plotFigure(data, label, cmap='Blues'):
 #Minnor slicing on borders to enhance colobars
 elevDem=grid.dem[:-1,:-1]
 plotFigure(elevDem, 'Elevation (m)')
+plt.show()
 
 # Detect depressions
 depressions = grid.detect_depressions('dem')
 
 # Plot depressions
 plt.imshow(depressions)
+plt.show()
+
 
 # Fill depressions
 grid.fill_depressions(data='dem', out_name='flooded_dem')
 # Test result
 depressions = grid.detect_depressions('flooded_dem')
 plt.imshow(depressions)
+plt.show()
+
 # Detect flats
 flats = grid.detect_flats('flooded_dem')
 
 # Plot flats
 plt.imshow(flats)
+plt.show()
 
 grid.resolve_flats(data='flooded_dem', out_name='inflated_dem')
 plt.imshow(grid.inflated_dem[:-1,:-1])
+plt.show()
+
 
 # Create a flow direction grid
 #N    NE    E    SE    S    SW    W    NW
 dirmap = (64,  128,  1,   2,    4,   8,    16,  32)
 grid.flowdir(data='inflated_dem', out_name='dir', dirmap=dirmap)
 plotFigure(grid.dir,'Flow Direction','viridis')
+plt.show()
+
 
 # Delineate the catchment
 grid.catchment(data='dir', x=x, y=y, dirmap=dirmap, out_name='catch',
@@ -102,16 +116,16 @@ plt.show()
 
 
 ## Copy/get ice thickness files for each glacier
-
-glaciers_catchment.drop('geometry', axis=1).to_csv('/home/ana/Desktop/csv_test.csv', index=False)
+glaciers_catchment.drop('geometry', axis=1).to_csv(working_directory + 'rgi_csv.csv', index=False) # gets deleted after
 
 # here code for the shell script
-os.system('./Catchment/ice_thickness.sh')
+os.chdir(path=working_directory)
+subprocess.call(['./ice_thickness', ice_thickness_files, output_test])
 
-subprocess.call(['sh', './home/ana/Desktop/ice_thickness.sh'])
 
 ## create shapefile with different elevation bands/lines and cut this to the glacier shapefiles
 
+
 ## apply contour lines on ice thickness files and calculate mass per band
 
-## combine into elevation zones and create dataframe
+## combine into elevation zones and create dataframe.
