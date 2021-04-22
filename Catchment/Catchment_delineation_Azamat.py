b2b18bed5a30a7d71e88ce22495ff00c7d9839bd
Ana-Lena Tappe <ana-lena.tappe@hu-berlin.de>
2021-04-22 16:40:32 +0200

    CMIP

--- Catchment/Catchment_delineation_Azamat.py
+++ Catchment/Catchment_delineation_Azamat.py
@@ -11,14 +11,13 @@ warnings.filterwarnings('ignore')
 
 ##
 # DEM_file = home + '/Seafile/Ana-Lena_Phillip/data/input_output/input/observations/karabatkak/static/ALPSMLC30_N042E078_DSM_glacier_clip.tif'
-DEM_file = home + '/Seafile/Ana-Lena_Phillip/data/input_output/input/observations/karabatkak/static/srtm_glacier_clip.tif'
+DEM_file = home + '/Seafile/Ana-Lena_Phillip/data/input_output/input/observations/karabatkak/static/srtm_catchment_clip.tif'
 output_file = home + "/Seafile/Ana-Lena_Phillip/data/input_output/input/observations/karabatkak/"
 
 # Specify discharge point
-x, y = 78.06284861, 42.32793047 # hydro
-x2, y2 =  78.26573705, 42.16429283 # hydro glacier
+x, y = 78.06284861, 42.32793047 # hydro (klappt nicht)
+y2, x2 = 42.32703430, 78.06141496 # hydro klappt (selbst gewählt)
 
-x3, y3 = 78.26615367, 42.16260997
 
 ##
 grid = Grid.from_raster(DEM_file, data_name='dem')
@@ -32,38 +31,37 @@ def plotFigure(data, label, cmap='Blues'):
 #Minnor slicing on borders to enhance colobars
 elevDem = grid.dem[:-1,:-1]
 plotFigure(elevDem, 'Elevation (m)')
-# plt.show()
+plt.show()
 # Detect depressions
 depressions = grid.detect_depressions('dem')
 
 # Plot depressions
 plt.imshow(depressions)
-# plt.show()
+plt.show()
 # Fill depressions
 grid.fill_depressions(data='dem', out_name='flooded_dem')
 # Test result
 depressions = grid.detect_depressions('flooded_dem')
 plt.imshow(depressions)
-# plt.show()
+plt.show()
 # Detect flats
 flats = grid.detect_flats('flooded_dem')
 
 # Plot flats
 plt.imshow(flats)
-# plt.show()
+plt.show()
 grid.resolve_flats(data='flooded_dem', out_name='inflated_dem')
 plt.imshow(grid.inflated_dem[:-1,:-1])
-# plt.show()
+plt.show()
 # Create a flow direction grid
 #N    NE    E    SE    S    SW    W    NW
 dirmap = (64,  128,  1,   2,    4,   8,    16,  32)
 grid.flowdir(data='inflated_dem', out_name='dir', dirmap=dirmap)
 plotFigure(grid.dir,'Flow Direction','viridis')
-# plt.show()
+plt.show()
 # Delineate the catchment
-col, row = grid.nearest_cell(x3, y3)
-grid.catchment(data=grid.dir, x=col, y=row, dirmap=dirmap, out_name='catch',
-               recursionlimit=15000, xytype='index', nodata_out=0 )
+grid.catchment(data=grid.dir, x=x2, y=y2, dirmap=dirmap, out_name='catch',
+               recursionlimit=15000, xytype='label', nodata_out=0)
 # Clip the bounding box to the catchment
 grid.clip_to('catch')
 # Get a view of the catchment
@@ -80,7 +78,7 @@ schema = {
     'properties': {'LABEL': 'float:16'}
 }
 
-with fiona.open(output_file + "shapefile_hydro.shp", 'w',
+with fiona.open(output_file + "shapefile_hydro_kyzylsuu.shp", 'w',
                 driver='ESRI Shapefile',
                 crs=grid.crs.srs,
                 schema=schema) as c:
@@ -95,70 +93,4 @@ with fiona.open(output_file + "shapefile_hydro.shp", 'w',
 c.close()
 
 ##
-grid = Grid.from_raster(DEM_file, data_name='dem')
-
-#Define a function to plot the digital elevation model
-def plotFigure(data, label, cmap='Blues'):
-    plt.figure(figsize=(12,10))
-    plt.imshow(data, extent=grid.extent)
-    plt.colorbar(label=label)
-    plt.grid()
-#Minnor slicing on borders to enhance colobars
-elevDem=grid.dem[:-1,:-1]
-plotFigure(elevDem, 'Elevation (m)')
-
-# Detect depressions
-depressions = grid.detect_depressions('dem')
-
-# Plot depressions
-plt.imshow(depressions)
-# Fill depressions
-grid.fill_depressions(data='dem', out_name='flooded_dem')
-# Test result
-depressions = grid.detect_depressions('flooded_dem')
-plt.imshow(depressions)
-# Detect flats
-flats = grid.detect_flats('flooded_dem')
-
-# Plot flats
-plt.imshow(flats)
-grid.resolve_flats(data='flooded_dem', out_name='inflated_dem')
-plt.imshow(grid.inflated_dem[:-1,:-1])
-# Create a flow direction grid
-#N    NE    E    SE    S    SW    W    NW
-dirmap = (64,  128,  1,   2,    4,   8,    16,  32)
-grid.flowdir(data='inflated_dem', out_name='dir', dirmap=dirmap)
-plotFigure(grid.dir,'Flow Direction','viridis')
-
-# Delineate the catchment
-grid.catchment(data='dir', x=x2, y=y2, dirmap=dirmap, out_name='catch',
-               recursionlimit=15000, xytype='label', nodata_out=0)
-# Clip the bounding box to the catchment
-grid.clip_to('catch')
-# Get a view of the catchment
-#demView = grid.view('dem', nodata=np.nan)
-#plotFigure(demView,'Elevation')
-#plt.show()
-
-
-## Create shapefile and save it
-shapes = grid.polygonize()
-
-schema = {
-    'geometry': 'Polygon',
-    'properties': {'LABEL': 'float:16'}
-}
-
-with fiona.open(output_file + "shapefile_hydro_glacier_catchments.shp", 'w',
-                driver='ESRI Shapefile',
-                crs=grid.crs.srs,
-                schema=schema) as c:
-    i = 0
-    for shape, value in shapes:
-        rec = {}
-        rec['geometry'] = shape
-        rec['properties'] = {'LABEL' : str(value)}
-        rec['id'] = str(i)
-        c.write(rec)
-        i += 1
-c.close()
+print("Mean catchment elevation is " + str(np.nanmean(demView)) + " m")
