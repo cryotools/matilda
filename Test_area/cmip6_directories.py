d68bdecf18acb8a62d40b58c66bdcddd01d44279
Phillip Schuster <phillip.schuster@posteo.de>
2021-05-05 16:09:53 +0200

    Finished CMIP6 directory routine

--- Test_area/cmip6_directories.py
+++ Test_area/cmip6_directories.py
@@ -1,5 +1,10 @@
 import os
 
+wd = '/data/projects/ensembles/cmip6'
+# wd = '/home/phillip/Seafile/EBA-CA/Tianshan_data/CMIP/CMIP6/dir_test'
+var = ['near_surface_air_temperature', 'precipitation']
+scen = ['historical', 'ssp1_2_6', 'ssp2_4_5', 'ssp3_7_0', 'ssp5_8_5']     #  'historical', 'ssp1_2_6', 'ssp2_4_5', 'ssp3_7_0', 'ssp5_8_5'
+
 
 def list_files(directory):
     paths = []
@@ -13,32 +18,41 @@ def list_files(directory):
 
 
 def list_models(wd, var, scen):
-    models = []
+    models_scen = []
     if isinstance(var, list):
         if isinstance(scen, list):
             for v in var:
                 for s in scen:
+                    models = []
                     path = list_files(wd + '/' + v + '/' + s + '/')
                     for p in path:
                         mod = p.split(s)[1].split('/')
                         models.append(mod[len(mod) - 2])
-            models = list(set([i for i in models if models.count(i) >= len(var)*len(scen)]))
+                        models = list(set(models))
+                    models_scen = models_scen + models
+            models_scen = list(set([i for i in models_scen if models_scen.count(i) >= len(var)*len(scen)]))
 
         else:
             for v in var:
+                models = []
                 path = list_files(wd + '/' + v + '/' + scen + '/')
                 for p in path:
                     mod = p.split(scen)[1].split('/')
                     models.append(mod[len(mod) - 2])
-            models = list(set([i for i in models if models.count(i) >= len(var)]))
+                    models = list(set(models))
+                models_scen = models_scen + models
+            models_scen = list(set([i for i in models_scen if models_scen.count(i) >= len(var)]))
 
     elif isinstance(scen, list):
         for s in scen:
+            models = []
             path = list_files(wd + '/' + var + '/' + s + '/')
             for p in path:
                 mod = p.split(s)[1].split('/')
                 models.append(mod[len(mod) - 2])
-        models = list(set([i for i in models if models.count(i) >= len(scen)]))
+                models = list(set(models))
+            models_scen =  models_scen + models
+        models_scen = list(set([i for i in models_scen if models_scen.count(i) >= len(scen)]))
 
     else:
         path = list_files(wd + '/' + var + '/' + scen + '/')
@@ -46,16 +60,19 @@ def list_models(wd, var, scen):
         for p in path:
             mod = p.split(scen)[1].split('/')
             models.append(mod[len(mod) - 2])
+        models_scen = list(set(models))
 
-    return models
+    return models_scen
 
-
-wd = '/data/projects/ensembles/cmip6'
-var = ['near_surface_air_temperature', 'precipitation']
-scen = ['ssp1_2_6', 'ssp2_4_5']     # 'historical' 'ssp1_2_6' 'ssp2_4_5' 'ssp3_7_0' 'ssp5_8_5'
-
-print(list_models(wd, var, scen))
-
+model_list = list_models(wd, var, scen)
+print(model_list)
+if 'historical' in scen:
+    print('\n' + 'A total of ' + str(len(model_list)) + ' models are available for the variable(s) ' +
+          ' and '.join(var) + ' under the scenario(s) ' + ' and '.join(scen) + '.' + '\n')
+else:
+    print('\n' + 'A total of ' + str(len(model_list)) + ' models are available for the variable(s) ' +
+          ' and '.join(var) + ' under the RCP(s) ' +
+          ' and '.join([i.split('_')[1] + '.' + i.split('_')[2] for i in scen]) + '.' + '\n')
 
 
 
