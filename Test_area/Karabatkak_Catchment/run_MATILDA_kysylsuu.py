cf92898c9c9a263c497b97e07f86f7eecc2e4f54
Phillip Schuster <phillip.schuster@posteo.de>
2021-05-28 13:41:30 +0200

    Finished downscaling module based on scikit-downscale

--- Test_area/Karabatkak_Catchment/run_MATILDA_kysylsuu.py
+++ Test_area/Karabatkak_Catchment/run_MATILDA_kysylsuu.py
@@ -11,7 +11,6 @@ import spotpy
 import numpy as np
 import socket
 import matplotlib.pyplot as plt
-from MATILDA_slim import MATILDA
 
 #mpl.use('Agg')
 
@@ -25,24 +24,37 @@ else:
 sys.path.append(home + '/Ana-Lena_Phillip/data/scripts/MATILDA_package_slim')
 sys.path.append(home + '/Ana-Lena_Phillip/data/scripts/Test_area')
 import mspot_cirrus
-
+from MATILDA_slim import MATILDA
 
 algorithm = 'rope'
 rep = 1000
 
 ## Setting file paths and parameters
 working_directory = home + "/Ana-Lena_Phillip/data/"
-input_path = home + "/Seafile/Ana-Lena_Phillip/data/input_output/input/observations/karabatkak/"
+input_path = home + "/Ana-Lena_Phillip/data/input_output/input/observations/karabatkak/"
 
 data_csv = "ERA5/20210313_42.25-78.25_kyzylsuu_awsq_1982_2019.csv"  # dataframe with columns T2 (Temp in Celsius), RRR (Prec in mm) and if possible PE (in mm)
-runoff_obs = "obs_kyzylsuu_runoff_1994_1997_zero.csv"  # Daily Runoff Observations in m³/s
+# runoff_obs = "obs_kyzylsuu_runoff_1994_1997_zero.csv"  # Daily Runoff Observations in m³/s
+runoff_obs = home + "/EBA-CA/Azamat_AvH/workflow/data/Runoff/obs_kyzylsuu_runoff_Hydromet.csv"
 #output_path = working_directory + "input_output/output/" + data_csv[4:21]
-output_path = "/home/ana/Desktop/Meeting/kyzylsuu"
+# output_path = "/home/ana/Desktop/Meeting/kyzylsuu"
+output_path = home + "/EBA-CA/Azamat_AvH/workflow/data/MATILDA_runs/" + "Kyzylsuu_testrun"
 
 df = pd.read_csv(input_path + data_csv)
-obs = pd.read_csv(input_path + runoff_obs)
+# obs = pd.read_csv(input_path + runoff_obs)
+obs = pd.read_csv(runoff_obs)
 # obs.set_index('Date', inplace=True)
 obs = obs[['Date', 'Qobs']]
+
+plt.ion()
+
+# Basic overview plot
+obs_fig = obs.copy()
+obs_fig.set_index('Date', inplace=True)
+obs_fig.index = pd.to_datetime(obs_fig.index)
+plt.figure()
+ax = obs_fig.plot(label='Kyzysuu (Hydromet)')
+ax.set_ylabel('Discharge [m³/s]')
 
 ## Parametrization
 
@@ -90,9 +102,9 @@ else:
 #                                       area_cat=315.69, area_glac=2.95,
 #                                       ele_dat=2550, ele_glac=3957, ele_cat=3221)
 # ## Running MATILDA
-parameter = MATILDA.MATILDA_parameter(df, set_up_start='1993-01-01 00:00:00', set_up_end='1993-12-31 23:00:00',
-                                      sim_start='1994-01-01 00:00:00', sim_end='1997-12-31 23:00:00', freq="W",
-                                      area_cat=315.69, area_glac=32.54,
+parameter = MATILDA.MATILDA_parameter(df, set_up_start='1989-01-01 00:00:00', set_up_end='1991-12-31 23:00:00',
+                                      sim_start='1989-01-01 00:00:00', sim_end='2019-12-31 23:00:00', freq="W",
+                                      area_cat=315.69, area_glac=32.54, lat=42.33,
                                       ele_dat=2550, ele_glac=4000, ele_cat=3221, lr_temp=-0.005936, lr_prec=-0.0002503,
                                       TT_snow=0.354, TT_rain=0.5815, CFMAX_snow=4.824, CFMAX_ice=5.574, CFR_snow=0.08765,
                                       CFR_ice=0.01132, BETA=2.03, CET=0.0471, FC=462.5, K0=0.03467, K1=0.0544, K2=0.1277,
