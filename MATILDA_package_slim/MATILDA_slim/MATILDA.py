1b9bce848afd8875987be850752b9ce2e39d4c52
Ana-Lena Tappe <ana-lena.tappe@hu-berlin.de>
2021-06-08 16:53:12 +0200

    plot text fix

--- MATILDA_package_slim/MATILDA_slim/MATILDA.py
+++ MATILDA_package_slim/MATILDA_slim/MATILDA.py
@@ -946,10 +946,10 @@ def MATILDA_plots(output_MATILDA, parameter):
             ax1.fill_between(plot_data.index.to_pydatetime(), plot_data["Q_HBV"], plot_data["Q_Total"], color='#CC79A7',
                              alpha=.75, label="")
         ax1.set_ylabel("Runoff [mm]", fontsize=9)
-        if output_MATILDA[1] == "error":
-            anchored_text = AnchoredText('NS coeff exceeds boundaries', loc=2, frameon=False)
-        elif isinstance(output_MATILDA[1], float):
+        if isinstance(output_MATILDA[1], float):
             anchored_text = AnchoredText('NS coeff ' + str(round(output_MATILDA[1], 2)), loc=1, frameon=False)
+        else:
+            anchored_text = AnchoredText('NS coeff exceeds boundaries', loc=2, frameon=False)
         ax1.add_artist(anchored_text)
         if 'Qobs' in plot_annual_data.columns:
             ax2.plot(plot_annual_data.index.to_pydatetime(), plot_annual_data["Qobs"], c="#E69F00",
