fb91785c187bcd943be76d43e82478ddcb2b98f8
Correct Name <analenatappe@gmail.com>
2021-06-10 16:44:10 +0200

    MATILDA plot fix

--- Preprocessing/glacier_profile_build.py
+++ Preprocessing/glacier_profile_build.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 
 ##
-gis_thickness = "/Seafile/Ana-Lena_Phillip/data/input_output/input/observations/karabatkak/kyzulsuu_glacier_profil.csv"
-catchment_area = 315.69
+gis_thickness = "/Seafile/Papers/No1_Kysylsuu_Bash-Kaingdy/data/GIS/Kysylsuu/thickness_mean.csv"
+catchment_area = 315.694
 elezone_interval = 100
 def round_elezones(x, base=100):
     return base * round(x/base)
@@ -21,6 +21,6 @@ glacier_profile["EleZone"] = round_elezones(glacier_profile["Elevation"], base=e
 glacier_profile = glacier_profile.sort_values(by='Elevation',ascending=True).reset_index(drop=True)
 
 ##
-glacier_profile.to_csv(home + gis_thickness, index=False)
+glacier_profile.to_csv("/home/ana/Seafile/Papers/No1_Kysylsuu_Bash-Kaingdy/data/kyzulsuu_glacier_profile.csv", index=False)
 
 
