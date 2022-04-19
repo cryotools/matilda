f5bab96ed2d9e96f7fe0d9062cb560e2a4ca0a67
Phillip Schuster <phillip.schuster@posteo.de>
2022-04-19 14:46:10 +0200

    Clean-up

--- Preprocessing/Downscaling/ERA5_downscaling/run_aws2cosipy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Run aws2python routine on input_cosipy.csv and static.nc to produce input_cosipy.nc
-sudo python3 /home/ana/Seafile/SHK/Scripts/cosipy/utilities/aws2cosipy/aws2cosipy.py -c /home/ana/Seafile/Ana-Lena_Phillip/data/input_output/input/20200810_Umrumqi_ERA5_2000_2019.csv -o /home/ana/Seafile/Ana-Lena_Phillip/input_output/input/20200810_Umrumqi_ERA5_2000_2019_cosipy.nc -s /home/ana/Seafile/Ana-Lena_Phillip/data/input_output/static/Urumqi_static.nc
-
-
--- Tools/Quantile_Mapping/Read_me.txt
+++
@@ -1,2 +0,0 @@
-See 'Halji_downscaling_ERA5_land.py' line 131-137
---> writes a dataframe which is then loaded into R, edited using 'Halji_Qmap.R' and re-imported in python
