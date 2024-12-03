a8de4fa5c9a2c844f5864025f2b126b168059292
Phillip Schuster <phillip.schuster@posteo.de>
2024-12-03 14:05:00 +0100

    Version update

--- matilda/__init__.py
+++ matilda/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 '''
-Copyright (c) 2022 by Phillip Schuster
+Copyright (c) 2024 by Phillip Schuster
 This file is part of the framework for Modeling Water Resources in Glacierized Catchments (MATILDA).
 
-:author: Phillip Schuster & Ana-Lena Tappe
+:author: Phillip Schuster, Ana-Lena Tappe & Alexander Georgi
 
 This package combines a positive degree-day model for glacier and snow melt with an adapted version of
 the lumped conceptual catchment model HBV. It further features a glacier area-volume scaling routine to account for
@@ -25,5 +25,5 @@ glacier changes.
 '''
 from . import core, mspot_glacier  # Contains all core functions
 
-__version__ = '0.3'
+__version__ = '1.0'
 
--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="matilda",
-    version="0.3",
+    version="1.0",
     author="Phillip Schuster, Ana-Lena Tappe & Alexander Georgi",
     author_email="phillip.schuster@geo.hu-berlin.de",
     packages=["matilda"],
     description="A package to model water resources in glacierized catchments",
     long_description="Tool for modeling water resources in glacierized catchments. Combines a temperature-index melt model with the conceptual catchment model HBV and a parameterized glacier area/volume re-scaling routine.",
     long_description_content_type="text/markdown",
-    url="https://github.com/cryotools/matilda/matilda_v0.3/matilda",
+    url="https://github.com/cryotools/matilda",
     license='MIT',
     python_requires='>=3.11',
     install_requires=[
