1db34aa161313b406bdac87355cb5946131afc57
hoepke <miajanzen@gmx.de>
2025-06-24 14:16:25 +0200

    added manual

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'matilda'
+copyright = '2025, Phillip Schuster, Alexander Georgi, Ana-Lena Tappe'
+author = 'Phillip Schuster, Alexander Georgi, Ana-Lena Tappe'
+release = '1.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',           # Markdown-Unterstützung
+    'sphinx.ext.autodoc',    # falls du später Python-Code dokumentieren willst
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
---
+++ docs/index.rst
@@ -0,0 +1,13 @@
+.. matilda documentation master file, created by
+   sphinx-quickstart on Tue Jun 24 12:14:37 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+matilda documentation
+=====================
+
+.. toctree::
+   :maxdepth: 0
+
+   readme
+
