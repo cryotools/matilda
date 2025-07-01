5aab1a08765a3f78ae1180cb31c4723ddb0409e0
hoepke <miajanzen@gmx.de>
2025-07-01 14:36:41 +0200

    can now read LaTex

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,12 @@ extensions = [
     'myst_parser',        
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'myst_parser',
+    'sphinx.ext.mathjax'
 ]
+
+myst_enable_extensions = ["dollarmath"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
