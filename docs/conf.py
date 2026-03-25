40b8886d4e3d2188e761bb9be2fcde0f73f02b4c
hoepke <miajanzen@gmx.de>
2026-03-25 10:14:57 +0100

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = '_static/logo.png'
 html_static_path = ['_static']
 
 source_suffix = {
